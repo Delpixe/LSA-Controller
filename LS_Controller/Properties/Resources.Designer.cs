83439a6ece23a7d1829dba4118fa40d4113b23aa
Mattia Del Papa <m.delpapa1@campus.uniurb.it>
2020-03-05 10:22:45 +0100

    Aggiunte Icone prova

--- LS_Controller/Properties/Resources.Designer.cs
+++ LS_Controller/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LS_Controller.Properties {
+namespace LSController.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace LS_Controller.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ExportToNewSyntax.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LSController.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
